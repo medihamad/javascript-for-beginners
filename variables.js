35454f21862b3bd0a6b7a5b324a64d3b91724c1d
Medi Hamad <kumarpatel692@gmail.com>
2021-05-10 07:11:13 +0300

    working with variables

---
+++ variables.js
@@ -0,0 +1,24 @@
+/*
+date: 10 May 2021
+author: Medi Hamad
+
+In this section we are goint to work with variables in javascript, declaring, initializing and outputing
+
+KEYWORD FOR VARIABLE ARE:
+
+var, let, const 
+
+*/
+
+//declaring a variable
+
+var firstName = 'Medi';
+
+// printing the variable name in the console.
+
+console.log(firstName);
+
+//with var and let we can reassign variables
+
+firstName = 'Hamad';
+console.log(firstName);
